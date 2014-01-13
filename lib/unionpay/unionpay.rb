33a1715048a625df32cd108e35145d8dab0ad22b
Weston Wei <weixuhong@rongyi.com>
2014-01-13 18:25:48 +0800

    conver tab to space

--- lib/unionpay/unionpay.rb
+++ lib/unionpay/unionpay.rb
@@ -1,38 +1,38 @@
 module Unionpay
-	module Unionpay
-		def self.included(receiver)
-			receiver.send(:include, InstanceMethods) 
-			receiver.send(:extend, ClassMethods)
-		end
+  module Unionpay
+    def self.included(receiver)
+      receiver.send(:include, InstanceMethods) 
+      receiver.send(:extend, ClassMethods)
+    end
 
-		module ClassMethods
+    module ClassMethods
 
-			def env=(env)
-				@env ||= env 
-			end
+      def env=(env)
+        @env ||= env 
+      end
 
-			def env
-				@env  
-			end
+      def env
+        @env  
+      end
 
-			def render_form(params)
-				::Unionpay::Helpers::Html.render_form(params)
-			end
+      def render_form(params)
+        ::Unionpay::Helpers::Html.render_form(params)
+      end
 
-			def config
-				@config ||= Config.new
-			end
+      def config
+        @config ||= Config.new
+      end
 
-			def setup
-				yield config
-			end
+      def setup
+        yield config
+      end
 
-		end
+    end
 
-		module InstanceMethods
-		end
+    module InstanceMethods
+    end
 
-	end
+  end
 end
 
 
